09886f01ce42880ef5b795485054e1ff54ab7786
[sangjin] <sangjincha719@gmail.com>
2023-01-11 22:02:47 +0900

    route_set

--- src/App.js
+++ src/App.js
@@ -1,73 +1,76 @@
-
-import './App.css';
-
-
-import {Col, Row, Nav, Navbar, Container} from 'react-bootstrap';
-import iu from "./iu.jpg"
-import { useState } from 'react';
+//여러 소스들
+import "./App.css";
+import iu from "./iu.jpg";
 import data from "./data";
-import { func } from 'prop-types';
 
+//컴포넌트
+import Detail from "./components/Detail";
+
+//리액트 관련
+import { Col, Row, Nav, Navbar, Container } from "react-bootstrap";
+import { useState } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+
+let links = ["https://codingapple1.github.io/shop/shoes1.jpg", "https://codingapple1.github.io/shop/shoes2.jpg", "https://codingapple1.github.io/shop/shoes3.jpg"];
 
 function App() {
-
   let [shoes, setShoes] = useState(data);
-
 
   return (
     <div className="App">
-      <ColorSchemesExample/>
-
-      <div className='main-bg'></div>
-
-      <div>
-      <Row>
-        <Items/>
-      </Row>
-      </div>
-
+      <ColorSchemesExample />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/detail" element={<Detail i={0} />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
 
+function Main() {
+  return (
+    <div>
+      <div className="main-bg"></div>
+      <div>
+        <Row>
+          <Items />
+        </Row>
+      </div>
+    </div>
+  );
+}
+
 function Items() {
   let result = data.map((value, i) => {
-    
-    return (
-      <Item index={i} shoes={value} Key={i}></Item>
-    )
-  })
+    return <Item shoes={value} Key={i}></Item>;
+  });
   return result;
 }
 
-
 function Item(props) {
-
-  let i = props.index;
+  let i = props.Key;
   let shoes = props.shoes;
 
   return (
     <Col>
-      <img src=''/>
+      <img id="card" src={links[i]} />
       <h4>{shoes.title}</h4>
       <p>{shoes.price}</p>
     </Col>
-
-  )
+  );
 }
-
 
 function ColorSchemesExample() {
   return (
     <>
       <Navbar bg="primary" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/detail">Detail</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
@@ -75,4 +78,3 @@ function ColorSchemesExample() {
     </>
   );
 }
-
