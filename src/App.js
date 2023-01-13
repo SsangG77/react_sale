1ca7b67dfb111a9b50bfa16ac8c0b012c511ff68
[sangjin] <sangjincha719@gmail.com>
2023-01-13 23:06:31 +0900

    url파라미터_styled_components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import data from "./data";
 
 //컴포넌트
 import Detail from "./routes/Detail";
+import About from "./routes/about";
 
 //리액트 관련
 import { Col, Row, Nav, Navbar, Container } from "react-bootstrap";
@@ -22,7 +23,7 @@ function App() {
       <ColorSchemesExample />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/detail" element={<Detail shoes={shoes} />} />
+        <Route path="/detail/:i" element={<Detail shoes={shoes} />} />
         <Route
           path="/event"
           element={
@@ -35,6 +36,7 @@ function App() {
           <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>}></Route>
           <Route path="two" element={<div>생일기념 쿠폰받기</div>}></Route>
         </Route>
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
@@ -83,8 +85,9 @@ function ColorSchemesExample() {
           <Navbar.Brand href="/">Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/detail">Detail</Nav.Link>
+            <Nav.Link href="/detail/0">Detail</Nav.Link>
             <Nav.Link href="/event">Event</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,16 +1,33 @@
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
 let links = ["https://codingapple1.github.io/shop/shoes1.jpg", "https://codingapple1.github.io/shop/shoes2.jpg", "https://codingapple1.github.io/shop/shoes3.jpg"];
 
+let Btn = styled.button`
+  background: ${(props) => props.bg};
+  color: black;
+  padding: 10px;
+`;
+
+let BlackBox = styled.div`
+  background: black;
+  color: black;
+  width: 60px;
+  height: 60px;
+`;
+
 function Detail(props) {
+  let { i } = useParams();
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6">
-          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+          <img src={links[i]} width="100%" />
         </div>
         <div className="col-md-6 mt-4">
-          <h4 className="pt-5">{props.shoes[0].title}</h4>
-          <p>{props.shoes[0].content}</p>
-          <p>{props.shoes[0].price}원</p>
+          <h4 className="pt-5">{props.shoes[i].title}</h4>
+          <p>{props.shoes[i].content}</p>
+          <p>{props.shoes[i].price}원</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
