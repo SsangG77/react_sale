a4e7308fa6bd608fb7393e3d8f475757d51a3774
[sangjin] <sangjincha719@gmail.com>
2023-01-24 19:43:09 +0900

    redux_state변경함수

--- src/routes/Basket.js
+++ src/routes/Basket.js
@@ -1,23 +1,22 @@
 //컴포넌트
-import { Table } from "react-bootstrap";
+
 import { useSelector, useDispatch } from "react-redux";
 
-import { changeName, countUp } from "../store";
+import { changeName, countUp, increase } from "../store.js";
 
 export function Basket() {
   let cart = useSelector((state) => {
     return state.cart;
   });
-  let state = useSelector((state) => {
-    return state;
-  });
-  console.log(cart);
+
+  let state = useSelector((state) => state);
+  console.log(state.cart);
 
   let dispatch = useDispatch();
 
   return (
     <>
-      <span>{state.user}</span>
+      <span>{state.user.name}</span>
       <button
         onClick={() => {
           dispatch(changeName(state.user));
@@ -25,42 +24,36 @@ export function Basket() {
       >
         Click
       </button>
+      <span>{state.user.age}</span>
+      <button
+        onClick={() => {
+          dispatch(increase());
+        }}
+      >
+        Age Up
+      </button>
+
       <table className="table table-hover">
         <thead>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">상품명</th>
+            <th scope="col">수량</th>
+            <th scope="col">변경하기</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          {cart.map((item, i) => {
+          {state.cart.map((item, i) => {
             return (
               <tr key={i}>
-                <th scope="row" key={i}>
-                  {item.id}
-                </th>
-                <td>{item.name}</td>
-                <td>{item.count}</td>
-                <td>@sangjin</td>
+                <td>1</td>
+                <td>{state.cart[i].name}</td>
+                <td>{state.cart[i].count}</td>
+
                 <td>
                   <button
                     onClick={() => {
-                      dispatch(countUp(cart[i].count));
-                      console.log(cart[i].count);
+                      dispatch(countUp(i));
                     }}
                   >
                     +
